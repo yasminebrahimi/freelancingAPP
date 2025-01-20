720bd65bc45cbc467245f595e5ae9cd323cddc11
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2025-01-20 19:42:06 +0200

    tailwind css setup

---
+++ freelnacer-app-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+  <h1 className="container xl:max-w-screen-xl text-4xl bg-opacity-80 text-red-300 bg-primary-900"> 
+  Starting point 
+  </h1>
+  ); 
+}
+
+
+
+export default App; 
